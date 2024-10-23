f71c4d60fa3c52bc20ce8b3d983714d83bde5a7f
Johao David Gavilánes Farias <johao.gavilanes@udla.edu.ec>
2024-10-22 22:04:23 -0500

    FUNCIONALIDADES

--- GAVILANESJOHAOTALLEREQUIPOS/Migrations/20241021182301_SegundaMigration.Designer.cs
+++ GAVILANESJOHAOTALLEREQUIPOS/Migrations/20241021182301_SegundaMigration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GAVILANESJOHAOTALLEREQUIPOS.Migrations
 {
     [DbContext(typeof(tallergavilanesContext))]
-    partial class tallergavilanesContextModelSnapshot : ModelSnapshot
+    [Migration("20241021182301_SegundaMigration")]
+    partial class SegundaMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GAVILANESJOHAOTALLEREQUIPOS/Migrations/20241021182301_SegundaMigration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GAVILANESJOHAOTALLEREQUIPOS.Migrations
+{
+    /// <inheritdoc />
+    public partial class SegundaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Posicion",
+                table: "Jugador",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Jugador",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Posicion",
+                table: "Jugador",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Jugador",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70);
+        }
+    }
+}
